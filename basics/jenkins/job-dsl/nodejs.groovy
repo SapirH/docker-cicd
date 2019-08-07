930d22798de8ce724dc7dad8b6641ecb6ea8857d
sapirH <sapir.holzman@gmail.com>
2019-08-07 11:41:11 +0200

    blkaaaaaaaaaaa

--- basics/jenkins/job-dsl/nodejs.groovy
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -35,7 +35,7 @@ job('NodeJS Docker example') {
             repositoryName('SapirH/docker-cicd') //qa / dev
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
-            buildContexts('./basic')
+            buildContext('./basic')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
